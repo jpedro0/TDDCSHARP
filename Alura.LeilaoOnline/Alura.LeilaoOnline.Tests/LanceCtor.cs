9518b6a96bccb37bfee7b0139b256a8d8aa4d540
Joao Pedro Brito <jpedro0brito@gmail.com>
2019-12-23 20:30:34 -0300

    Add teste para o valor do lance nao pode se menor que zero

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LanceCtor.cs
@@ -0,0 +1,24 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LanceCtor
+    {
+        [Fact]
+        public void LanceArgumentExceptionDadoValorNegativo()
+        {
+            //Arranje
+            var valorNegativo = -100;
+
+            //Assert
+            Assert.Throws<System.ArgumentException>(
+             //Act   
+             () => new Lance(null, valorNegativo)
+            );
+        }
+    }
+}
